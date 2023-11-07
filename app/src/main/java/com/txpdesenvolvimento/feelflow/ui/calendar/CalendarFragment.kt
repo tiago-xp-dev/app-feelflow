30bd0edfb627ac25cc5b56ce8e685f8be6ee8270
Tiago XP <tiagoxp.desenvolvedor@gmail.com>
2023-11-07 00:58:44 -0300

    SS

--- app/src/main/java/com/txpdesenvolvimento/feelflow/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/com/txpdesenvolvimento/feelflow/ui/calendar/CalendarFragment.kt
@@ -6,10 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TableRow
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import com.txpdesenvolvimento.feelflow.R
 import com.txpdesenvolvimento.feelflow.databinding.FragmentCalendarBinding
-import com.txpdesenvolvimento.feelflow.databinding.FragmentHomeBinding
 
 class CalendarFragment : Fragment() {
 
@@ -30,13 +32,24 @@ class CalendarFragment : Fragment() {
         _binding = FragmentCalendarBinding.inflate(inflater, container, false)
         val root : View = binding.root
 
-        val field: TextView = binding.txtYear
+        val table = binding.tableLayout
 
-        viewModel.year.observe(viewLifecycleOwner) {
-            field.text = it.toString()
+        for (i in 0 until  12 step 1){
+            val row = TableRow(root.context)
+
+            val v1 = layoutInflater.inflate(R.layout.fragment_year_in_pixel_day, null)
+            val v2 = layoutInflater.inflate(R.layout.fragment_year_in_pixel_day, null)
+            val v3 = layoutInflater.inflate(R.layout.fragment_year_in_pixel_day, null)
+            val v4 = layoutInflater.inflate(R.layout.fragment_year_in_pixel_day, null)
+
+            row.addView(v1)
+            row.addView(v2)
+            row.addView(v3)
+            row.addView(v4)
+
+            table.addView(row)
         }
 
         return root
-        //return inflater.inflate(R.layout.fragment_calendar, container, false)
     }
 }
